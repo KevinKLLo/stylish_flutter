8cb94f8b2283aff582b93219e99b36864bafde15
KevinLo <wind19891002@gmail.com>
2023-04-05 19:54:19 +0800

    [Feat] 更新 DetailVC UI

--- lib/detail.dart
+++ lib/detail.dart
@@ -21,16 +21,92 @@ class DetailPage extends StatelessWidget {
           width: 320,
           child: SingleChildScrollView(
             padding: const EdgeInsets.only(top: 24, bottom: 24),
-            child: Column(
+            child: ListBody(
               children: [
                 Container(
                   height: 450,
                   color: Colors.grey,
                 ),
                 const SizedBox(height: 16),
-                Container(
+                SizedBox(
                   height: 450,
-                  color: Colors.grey,
+                  width: 320,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('UNIQLO 特級輕羽絨外套'),
+                      Text('2023032101'),
+                      SizedBox(height: 16),
+                      Text('NT\$ 323'),
+                      Divider(),
+                      Row(
+                        children: [
+                          Text('顏色'),
+                          VerticalDivider(),
+                          Container(
+                            width: 18,
+                            height: 18,
+                            color: Colors.red,
+                          ),
+                          SizedBox(width: 14),
+                          Container(
+                            width: 18,
+                            height: 18,
+                            color: Colors.blue,
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        children: [
+                          Text('顏色'),
+                          VerticalDivider(),
+                          Container(
+                            width: 18,
+                            height: 18,
+                            color: Colors.red,
+                          ),
+                          SizedBox(width: 14),
+                          Container(
+                            width: 18,
+                            height: 18,
+                            color: Colors.blue,
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        children: [
+                          Text('顏色'),
+                          VerticalDivider(),
+                          Container(
+                            width: 18,
+                            height: 18,
+                            color: Colors.red,
+                          ),
+                          SizedBox(width: 14),
+                          Container(
+                            width: 18,
+                            height: 18,
+                            color: Colors.blue,
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero),
+                          ),
+                          child: const Text('請選擇尺寸'),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),

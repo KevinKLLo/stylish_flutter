b213f8fa1313e346a0729b7c1ca4de8568fa034d
KevinLo <wind19891002@gmail.com>
2023-04-06 11:20:08 +0800

    [Feat] 建立 productInfo

--- lib/detail.dart
+++ lib/detail.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
   final String item;
+  final List<String> content = [
+    '實品顏色依照單品顏色為主',
+    '棉 100%',
+    '厚薄：薄',
+    '彈性：無',
+    '素材產地 / 日本',
+    '加工產地 / 中國'
+  ];
 
-  const DetailPage({required this.item});
+  DetailPage({
+    super.key,
+    required this.item,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -28,90 +39,118 @@ class DetailPage extends StatelessWidget {
                   color: Colors.grey,
                 ),
                 const SizedBox(height: 16),
-                SizedBox(
-                  height: 450,
-                  width: 320,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('UNIQLO 特級輕羽絨外套'),
-                      Text('2023032101'),
-                      SizedBox(height: 16),
-                      Text('NT\$ 323'),
-                      Divider(),
-                      Row(
-                        children: [
-                          Text('顏色'),
-                          VerticalDivider(),
-                          Container(
-                            width: 18,
-                            height: 18,
-                            color: Colors.red,
-                          ),
-                          SizedBox(width: 14),
-                          Container(
-                            width: 18,
-                            height: 18,
-                            color: Colors.blue,
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 16),
-                      Row(
-                        children: [
-                          Text('顏色'),
-                          VerticalDivider(),
-                          Container(
-                            width: 18,
-                            height: 18,
-                            color: Colors.red,
-                          ),
-                          SizedBox(width: 14),
-                          Container(
-                            width: 18,
-                            height: 18,
-                            color: Colors.blue,
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 16),
-                      Row(
-                        children: [
-                          Text('顏色'),
-                          VerticalDivider(),
-                          Container(
-                            width: 18,
-                            height: 18,
-                            color: Colors.red,
-                          ),
-                          SizedBox(width: 14),
-                          Container(
-                            width: 18,
-                            height: 18,
-                            color: Colors.blue,
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 16),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadius.zero),
-                          ),
-                          child: const Text('請選擇尺寸'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                ProductInfo(content: content),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ProductInfo extends StatelessWidget {
+  const ProductInfo({
+    super.key,
+    required this.content,
+  });
+
+  final List<String> content;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 320,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('UNIQLO 特級輕羽絨外套'),
+          Text('2023032101'),
+          SizedBox(height: 16),
+          Text('NT\$ 323'),
+          Divider(),
+          Row(
+            children: [
+              Text('顏色'),
+              VerticalDivider(),
+              Container(
+                width: 18,
+                height: 18,
+                color: Colors.red,
+              ),
+              SizedBox(width: 14),
+              Container(
+                width: 18,
+                height: 18,
+                color: Colors.blue,
+              )
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              Text('顏色'),
+              VerticalDivider(),
+              Container(
+                width: 18,
+                height: 18,
+                color: Colors.red,
+              ),
+              SizedBox(width: 14),
+              Container(
+                width: 18,
+                height: 18,
+                color: Colors.blue,
+              )
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              Text('顏色'),
+              VerticalDivider(),
+              Container(
+                width: 18,
+                height: 18,
+                color: Colors.red,
+              ),
+              SizedBox(width: 14),
+              Container(
+                width: 18,
+                height: 18,
+                color: Colors.blue,
+              )
+            ],
+          ),
+          const SizedBox(height: 16),
+          SizedBox(
+            width: double.infinity,
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero),
+              ),
+              child: const Text('請選擇尺寸'),
+            ),
+          ),
+          const SizedBox(height: 8),
+          ListBody(
+            children: content
+                .map(
+                  (text) => Text(
+                    text,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      height: 1.5,
+                    ),
+                  ),
+                )
+                .toList(growable: false),
+          )
+        ],
       ),
     );
   }

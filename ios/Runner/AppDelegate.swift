c836a831473a9ac877015672c266b3c96c42c380
KevinLo <wind19891002@gmail.com>
2023-04-23 21:23:59 +0800

    點擊購物車按鈕串接 platform, 串接 iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,23 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+    let shoppingCartChannel = FlutterMethodChannel(name: "stylish_flutter/shoppingCart", binaryMessenger: controller.binaryMessenger)
+
+    // 2. 偵測呼叫的 method
+    shoppingCartChannel.setMethodCallHandler({
+        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        // This method is invoked on the UI thread.
+        // Handle battery messages.
+        switch call.method {
+        case "tapPurchase":
+            result("from iOS")
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/ui/shopping_cart.dart
+++ lib/ui/shopping_cart.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class ShoppingCart extends StatelessWidget {
+class ShoppingCart extends StatefulWidget {
   const ShoppingCart({super.key});
+
+  @override
+  State<ShoppingCart> createState() => _ShoppingCartState();
+}
+
+class _ShoppingCartState extends State<ShoppingCart> {
+  static const platform = MethodChannel('stylish_flutter/shoppingCart');
+  String _status = '預設狀態';
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +23,26 @@ class ShoppingCart extends StatelessWidget {
           height: 20,
         ),
       ),
-      body: const Center(
-        child: Text('購物車'),
+      body: Center(
+        child: Column(
+          children: [
+            const Text('購物車'),
+            ElevatedButton(
+              onPressed: _tapPurchase,
+              child: Text(_status),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _tapPurchase() async {
+    final String result =
+        await platform.invokeMethod('tapPurchase', {'from': 'Flutter'});
+
+    setState(() {
+      _status = '購買結果：$result';
+    });
   }
 }

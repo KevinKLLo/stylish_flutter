10dc7c217445472e12d0fb42b6e4ef6026213e10
KevinLo <wind19891002@gmail.com>
2023-04-28 18:37:19 +0800

    點擊 marker 出現 cardView, 點擊地圖還原

--- lib/ui/map.dart
+++ lib/ui/map.dart
@@ -11,6 +11,7 @@ class MapView extends StatefulWidget {
 class _MapViewState extends State<MapView> {
   late GoogleMapController _mapController;
   Map<MarkerId, Marker> markersMap = {};
+  String? _cardContent;
 
   final CameraPosition _center = const CameraPosition(
     target: LatLng(45.521563, -122.677433),
@@ -29,16 +30,39 @@ class _MapViewState extends State<MapView> {
         title: const Text('Maps Sample App'),
         elevation: 2,
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: _center,
-        onLongPress: _onLongPress,
-        markers: markersMap.values.toSet(),
-      ),
+      body: Stack(children: [
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: _center,
+          onTap: _onTap,
+          onLongPress: _onLongPress,
+          markers: markersMap.values.toSet(),
+        ),
+        Visibility(
+          visible: _cardContent != null,
+          child: Positioned(
+            left: 20,
+            right: 20,
+            bottom: 50,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(_cardContent ?? ''),
+              ),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 
-  // 長按地圖的動作
+  // 點擊地圖
+  void _onTap(LatLng latLng) {
+    _removeMarkers();
+    _removeCardConetnt();
+  }
+
+  // 長按地圖
   void _onLongPress(LatLng latLng) {
     _addMarker(latLng);
   }
@@ -50,17 +74,32 @@ class _MapViewState extends State<MapView> {
       markerId: markerId,
       position: latLng,
       infoWindow: InfoWindow(
-        title: 'title',
-        snippet: 'snippet: \nmarkerId: ${markerId.value}',
+        title: 'Title',
+        snippet: 'MarkerId: ${markerId.value}',
       ),
       onTap: () {
-        print(markerId);
+        setState(() {
+          _cardContent = markerId.value;
+        });
       },
     );
 
     setState(() {
-      markersMap.clear();
       markersMap[markerId] = marker;
+    });
+  }
+
+  // 移除 Markers
+  void _removeMarkers() {
+    setState(() {
+      markersMap.clear();
+    });
+  }
+
+  // 移除 CardConetnt
+  void _removeCardConetnt() {
+    setState(() {
+      _cardContent = null;
     });
   }
 }
